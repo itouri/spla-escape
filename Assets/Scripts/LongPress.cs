efc99dc48020495216293e61286f33ac3d4f5c37
itouri <k-akiyama@axsh.net>
2018-09-07 21:24:07 +0900

    init commit

---
+++ Assets/Scripts/LongPress.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+using UniRx.Triggers;
+
+public class LongPress : MonoBehaviour
+{
+	private GameObject _tween;
+
+	void Start()
+	{
+		_tween  = GameObject.Find("Tween");
+
+		var mouseDownStream = this.UpdateAsObservable().Where(_ => Input.GetMouseButtonDown(0));
+		var mouseUpStream = this.UpdateAsObservable().Where(_ => Input.GetMouseButtonUp(0));
+
+		//長押しの判定
+		//マウスクリックされたら3秒後にOnNextを流す
+		mouseDownStream
+			.SelectMany(_ => Observable.Timer(TimeSpan.FromSeconds(2)))
+			//途中でMouseUpされたらストリームをリセット
+			.TakeUntil(mouseUpStream)
+			.RepeatUntilDestroy(this.gameObject)
+			.Subscribe(_ => CallMove());
+
+		//長押しのキャンセル判定
+		mouseDownStream.Timestamp()
+			.Zip(mouseUpStream.Timestamp(), (d, u) => (u.Timestamp - d.Timestamp).TotalMilliseconds / 1000.0f)
+			.Where(time => time < 2.0f)
+			.Subscribe(t => Debug.Log(t + "秒でキャンセル"));
+	}
+
+	private void CallMove() {
+		_tween.GetComponent<Tween> ().Move ();
+	}
+}
---
+++ Assets/Scripts/Tween.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class Tween : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public void Move() {
+		var ts = this.transform;
+		ts.DOMove (
+			Vector3.one,
+			1.0f
+		).SetRelative ()
+		.OnStart(() => {
+			// アニメーションが終了時によばれる
+			Debug.Log("Start!");
+		})
+		.OnComplete(() => {
+			// アニメーションが終了時によばれる
+			Debug.Log("Complete!");
+		});
+	}
+
+//	public void ToAlpha() {
+//		var color = this.renderer.material.color
+//		// Alphaだけを変えたい場合はこちらも
+//		DOTween.ToAlpha(
+//			() => image.color,
+//			color => image.color = color,
+//			0f,                                // 最終的なalpha値
+//			3f
+//		);
+//	}
+}
